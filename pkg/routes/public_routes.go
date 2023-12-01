de709ca5831a0c7b05bca87ed9f7d5acc1ff0ab3
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-12-01 18:30:16 -0300

    Feat: create a file for the public routes

---
+++ pkg/routes/public_routes.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	"library-rest-api/app/controllers"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func PublicRoutes(a *fiber.App) {
+	route := a.Group("/api/v1")
+
+	route.Get("/books", controllers.GetBooks)
+	route.Get("/book/:id", controllers.GetBook)
+	route.Get("/token/new", controllers.GetNewAccessToken)
+}
